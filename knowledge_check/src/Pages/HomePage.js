9a01d2188a4de876a5af3186cd130470fabe7a86
Xvaughn623 <Xvaughn623@gmail.com>
2021-11-29 15:10:00 -0600

    In the middle of adding a navBar and working on the Home screen, just pushing changes so If someone needs to edit App.js they can easily

---
+++ knowledge_check/src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import {NavBar} from '../Components/NavBar.js';
+import './HomePage.css';
+
+const HomePage = () =>{
+return(
+<div>
+    Home Page
+    <div><NavBar fixed = "top"/></div>
+    <div>Home Page Temporary text</div>
+</div>
+)}
+export default HomePage;
