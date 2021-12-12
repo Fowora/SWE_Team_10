c7e4d2ede30f12f12e19beb71839c9b555b7eff2
Xvaughn623 <Xvaughn623@gmail.com>
2021-12-12 14:18:13 -0500

    just added the ability to map through entire array of Questions and show the metadata

---
+++ knowledge_check/src/Components/MockQuizMetaData.js
@@ -0,0 +1,20 @@
+const qData= [{
+    qName: "State Capitals",
+    author: "Professor Burge",
+    category: "Geography",
+    numQuestions: 1,
+},
+{
+    qName: "React Principles",
+    author: "X",
+    category: "Computer Science",
+    numQuestions: 2,
+},
+{
+    qName: "Discrete Structures Quiz 3",
+    author: "Professor Quill",
+    category: "Math",
+    numQuestions: 3,
+}];
+
+export {qData};
--- knowledge_check/src/Pages/AllQuizzes.js
+++ knowledge_check/src/Pages/AllQuizzes.js
@@ -1,14 +1,36 @@
 import './AllQuizzes.css';
 import {nameChange} from '../actions/Action.js';
 import {useDispatch} from 'react-redux';
+import {qData} from '../Components/MockQuizMetaData.js';
+
 //imports
 
+/*
+Example variable names
+qName: "React Principles",
+    author: "X",
+    category: "Computer Science",
+    numQuestions: 2,
+*/
 const AllQuizzes = () =>{
     const dispatch = useDispatch(); // Makes dispatch more readable
     dispatch(nameChange('All Quizzes')); //Calls the reducer to change the name of the page to Home
     
     return(
-    <div>All Quizes Page WIP</div>
+    <div>
+        <div className="AllQText">Featured Quizzes</div>
+        <div className="QuizModules">
+            {qData.map(quiz => { return(
+                <div className="outerQuizRectangle">
+                    <div className="quizContentText">{quiz.qName}</div>
+                    <div className="quizContentText">By: {quiz.author}</div>
+                    <div className="quizContentText">{quiz.category}</div>
+                    <div className="quizQNums">{quiz.numQuestions}</div>
+                </div>
+            )})}
+        </div>
+        
+    </div>
     )
 }
 export default AllQuizzes;
