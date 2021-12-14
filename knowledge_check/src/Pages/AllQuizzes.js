e8717fc10d2946d4ade0ad1b5ff8cfedd7c0d2f2
Javid Mitchell <javidmitchell16@gmail.com>
2021-12-13 23:33:46 -0500

    Revert "bruh"

--- knowledge_check/src/Pages/AllQuizzes.js
+++ knowledge_check/src/Pages/AllQuizzes.js
@@ -2,7 +2,6 @@ import './AllQuizzes.css';
 import {nameChange} from '../actions/Action.js';
 import {useDispatch} from 'react-redux';
 import {qData} from '../Components/MockQuizMetaData.js';
-import { Navigate, useNavigate } from 'react-router';
 
 //imports
 
@@ -17,25 +16,18 @@ const AllQuizzes = () =>{
     const dispatch = useDispatch(); // Makes dispatch more readable
     dispatch(nameChange('All Quizzes')); //Calls the reducer to change the name of the page to Home
     
-    const openQuiz = () =>{
-        let navigate = useNavigate();
-        navigate("./TakingAQuiz")
-    }
-
     return(
     <div>
         <div className="AllQText">Featured Quizzes</div>
         <div className="QuizModules">
-            {qData.map(quiz => { 
-                return(
-                <div className="outerQuizRectangle" onClick={() => openQuiz()}>
+            {qData.map(quiz => { return(
+                <div className="outerQuizRectangle">
                     <div className="quizContentText">{quiz.qName}</div>
                     <div className="quizContentText">By: {quiz.author}</div>
                     <div className="quizContentText">{quiz.category}</div>
                     <div className="quizQNums">{quiz.numQuestions}</div>
                 </div>
-                )
-            })}
+            )})}
         </div>
         
     </div>
